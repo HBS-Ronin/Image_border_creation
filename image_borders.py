7db8a5d8108a3bd45ee462b96a7509b230db31c6
Danyal Ahmad Siddiqui <danyalsiddiqui@Danyals-Laptop.local>
2023-05-31 20:50:59 +0100

    On branch main  Changes to be committed: 	modified:   image_borders.py 	added in functionality to do the following: 						1. cacluated borders based on minimum border thickness and aspect ratio 						2. can write to working directory 						3. took out the BGR2RGB conversion
    					TODO:   1. wrap the code in a class function
    						2. add in functionality to write images to folder adjacent from where they were taken

--- image_borders.py
+++ image_borders.py
@@ -5,11 +5,28 @@ import matplotlib.pyplot as plt
 dir = f'/Users/danyalsiddiqui/Pictures/Japan Edited Pictures'
 extension = f'/*.jpg'
 filenames = dir+extension
+asp_ratio = 4/5
+inv_asp_ratio = 1/asp_ratio
+min_bd_thi = 0.05
 
 jpg_fs = glob.glob(filenames)
 
-a = cv2.cvtColor(cv2.imread(jpg_fs[0]), cv2.COLOR_BGR2RGB)
-b = cv2.copyMakeBorder(a, 200, 200, 200, 200, cv2.BORDER_CONSTANT, value = (255, 255, 255))
+a = cv2.imread(jpg_fs[6])
+# max_dim = max(a.shape)
+if a.shape[0]>a.shape[1]:
+    top = bottom = round(min_bd_thi*a.shape[0])
+    left = right = round(((2*top + a.shape[0])*inv_asp_ratio - a.shape[0])*0.5) 
+elif a.shape[0]<a.shape[1]:
+    left = right = round(min_bd_thi*a.shape[1])
+    top = bottom = round(((2*left + a.shape[1])*asp_ratio - a.shape[0])*0.5)
+else:
+    top = bottom = round(min_bd_thi*a.shape[0])
+    right = left = round(((2*min_bd_thi+1)*a.shape[0]*inv_asp_ratio - a.shape[0])*0.5)
+
+b = cv2.copyMakeBorder(a, top, bottom, left, left, cv2.BORDER_CONSTANT, value = (255, 255, 255))
+cv2.imwrite('test_img.jpg', b)
+print(b.shape[0]/b.shape[1])
+
 
 plt.imshow(b)
 plt.show()
